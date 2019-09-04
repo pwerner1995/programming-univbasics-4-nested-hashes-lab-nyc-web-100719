16b49a085b0939628d4388f4e9e70858948a5fbd
Peter Werner <pcw1995@yahoo.com>
2019-09-04 20:30:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  string=programmer_hash[:dennis_ritchie][:languages]
-  string
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
